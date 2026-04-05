1214f384355a86af911b17f23d106f4a8277edaa
RogueMew <48804205+RogueMew@users.noreply.github.com>
2026-04-05 11:44:23 -0400

    Update collector.py

--- collector.py
+++ collector.py
@@ -5,32 +5,36 @@ def main():
     start = time.perf_counter()
     
     hollywood = WDWR.Park("Hollywood Studios", WDWR.ParkSlugs.hollywood)
+    print(f"Park times are {hollywood.openTime} - {hollywood.closeTime}")
     if hollywood.isParkOpen():
         print(f"Saving {hollywood.name} as a csv")
         hollywood.attractions.archiveToCSV(hollywood.name,hollywood.lastTimeCheck)
     else:
-        print(f"{hollywood.name} is closed")
+        print(f"{hollywood.name} is closed at {hollywood.closeTime}")
 
     dak = WDWR.Park("Animal Kingdom", WDWR.ParkSlugs.DAK)
+    print(f"Park times are {dak.openTime} - {dak.closeTime}")
     if dak.isParkOpen():
         print(f"Saving {dak.name} as a csv")
         dak.attractions.archiveToCSV(dak.name,dak.lastTimeCheck)
     else:
-        print(f"{dak.name} is closed")
+        print(f"{dak.name} is closed at {dak.closeTime}")
 
     magic = WDWR.Park("Magic Kingdom", WDWR.ParkSlugs.Magic)
+    print(f"Park times are {magic.openTime} - {magic.closeTime}")
     if magic.isParkOpen():
         print(f"Saving {magic.name} as a csv")
         magic.attractions.archiveToCSV(magic.name,magic.lastTimeCheck)
     else:
-        print(f"{magic.name} is closed")
+        print(f"{magic.name} is closed at {magic.closeTime}")
 
     epcot = WDWR.Park("EPCOT", WDWR.ParkSlugs.epcot)
+    print(f"Park times are {epcot.openTime} - {epcot.closeTime}")
     if epcot.isParkOpen():
         print(f"Saving {epcot.name} as a csv")
         epcot.attractions.archiveToCSV(epcot.name,epcot.lastTimeCheck)
     else:
-        print(f"{epcot.name} is closed")
+        print(f"{epcot.name} is closed at {epcot.closeTime}")
 
     
     end = time.perf_counter()
